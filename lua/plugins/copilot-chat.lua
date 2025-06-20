07a471bfc18ff941f74da036b07e8f9161baf36b
Daniela Patiño Benitez <daniela.patino@sequra.es>
2025-06-20 17:30:28 +0200

    update copilot model + rename help tags keymap for telescope

--- lua/plugins/copilot-chat.lua
+++ lua/plugins/copilot-chat.lua
@@ -1,16 +1,20 @@
 return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "main",
     dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "github/copilot.vim" }, -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
+    branch = "main",
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
       debug = true, -- Enable debugging
+      model = "claude-3.7-sonnet", -- Model to use
       -- See Configuration section for rest
     },
+    keys = {
+      { "<leader>kc", ":CopilotChat<CR>", mode = "n", desc = "Chat with Copilot" },
+    }
     -- See Commands section for default commands if you want to lazy load on them
   },
 }
