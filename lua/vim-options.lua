85422960ebd4fcda9f499b216e9e9ce7bd7c93db
Daniela Patiño Benitez <daniela.patino@sequra.es>
2026-03-09 09:27:31 +0100

    Remove whitespaces on save

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -12,3 +12,4 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth= 2
+vim.api.nvim_create_autocmd('BufWritePre', { pattern = '*', command = [[:%s/\s\+$//e]] })
